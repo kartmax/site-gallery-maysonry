3d8b8c31b4c96e6ded05b98291f593d0e68fb61c
Kartel Maksym <kartmax1804@gmail.com>
2024-10-28 16:18:25 +0100

    add open img

--- js/common.js
+++ js/common.js
@@ -10,3 +10,24 @@ window.onload = function() {
    }, 200);
 
 }
+
+
+// Открытие фото
+import PhotoSwipeLightbox from '../libs/PhotoSwipe/photoswipe-lightbox.esm.min.js';
+
+document.querySelectorAll('.gallery__item').forEach(linkImg => {
+    const img = new Image();
+    const urlImg = linkImg.href;
+
+    img.src = urlImg;
+
+    linkImg.dataset.pswpWidth = img.width;
+    linkImg.dataset.pswpHeight = img.height;
+})
+
+const lightbox = new PhotoSwipeLightbox({
+  gallery: '.gallery',
+  children: '.gallery__item',
+  pswpModule: () => import('../libs/PhotoSwipe/photoswipe.esm.min.js')
+});
+lightbox.init();
